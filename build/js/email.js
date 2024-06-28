3a4a6a30182945c7ae3975cf90b5489f06aad917
bigliuliu <1552000425@qq.com>
2024-06-28 15:15:02 +0800

    fixed contact component with emailJS

--- build/js/email.js
+++ build/js/email.js
@@ -4,21 +4,17 @@ function sendMail() {
     email: document.getElementById("email").value,
     message: document.getElementById("message").value,
   };
-
-  console.log(params);
-
-  // const serviceID = "service_ugy54y6";
-  // const templateID = "template_wwcvgbh";
-  
+  if (params.email === '' ||params.name === '' || params.message === ''){
+    alert("Please fill all inputs...")
+    return false
+  }
   const serviceID = "service_z5spwih";
-  const templateID = "template_exu518I";
-
+  const templateID = "template_exu518l";
     emailjs.send(serviceID, templateID, params)
     .then(res=>{
         document.getElementById("fullName").value = "";
         document.getElementById("email").value = "";
         document.getElementById("message").value = "";
-        console.log(res);
         alert("Your message sent successfully!!")
 
     })
