fbea5d96d929581134c5beaafb428adad8ade45e
bigliuliu <1552000425@qq.com>
2024-07-31 18:56:40 +0800

    add a youtube video player for the project of pearl town

--- build/js/menu.js
+++ build/js/menu.js
@@ -3,20 +3,18 @@ const menuIcon = document.querySelector("#menu");
 const list = document.querySelector(".toggle-list");
 
 menuIcon.addEventListener("click", () => {
-    menuToggle();
-    menuIcon.classList.toggle("scale-105");
-    list.addEventListener("click", () => {
-        dropdownMenu.classList.remove("flex");
-        dropdownMenu.classList.add("hidden");
-    })
-})
+  menuToggle();
+  menuIcon.classList.toggle("scale-105");
+  list.addEventListener("click", () => {
+    dropdownMenu.classList.remove("flex");
+    dropdownMenu.classList.add("hidden");
+  });
+});
 
 function menuToggle(params) {
-        dropdownMenu.classList.toggle("hidden");
-        dropdownMenu.classList.toggle("flex");
+  dropdownMenu.classList.toggle("hidden");
+  dropdownMenu.classList.toggle("flex");
 }
-
-
 
 //Projects Lists
 
@@ -44,7 +42,7 @@ const projectList = [
   //   githubLink: "",
   //   // live: "https://vetmedman.com",
   //   desc: "A shopping store I created for a freelance client. GithubRepo not available for this private project",
-   
+
   //  },
   //   {
   //   id: 3,
@@ -101,26 +99,53 @@ const projectList = [
 // function to return project-card
 const projectContainer = document.getElementById("project-container");
 
-function displayAllProjects(projectsList){
-    let displayProjects = projectsList.map(function(item){
-        return `
+function displayAllProjects(projectsList) {
+  let displayProjects = projectsList.map(function (item) {
+    if (item.id === 4) {
+      return `
         <div class="project-card" class="text-gray-300">
             <img src= ${item.img} alt=${item.title}>
             <h2 class="text-xl lg:text-2xl  font-bold mx-3 pt-1 underline underline-offset-8">${item.title}</h2>
             <div class="flex justify-between w-full px-4  projectLinkContainer items-center" >
               <p class="text-sm lg:text-lg">View Project</p>
-              <a href="${item.githubLink}" target="_blank"><img src="build/img/footer-github.png" alt="github-link""></a>
+            <a href="${item.githubLink}" target="_blank"><img src="build/img/footer-github.png" alt="github-link""></a>
             </div>
             <p class="m-1 w-4/5 text-xs lg:text-base">${item.desc}</p>
-        </div>`
-
+        </div>`;
+    } else if (item.id === 5) {
+      return `
+    <div class="project-card" class="text-gray-300">
+        <div id='player-1' ></div>
+        <h2 class="text-xl lg:text-2xl  font-bold mx-3 pt-1 underline underline-offset-8">${item.title}</h2>
+        <div class="flex justify-between w-full px-4  projectLinkContainer items-center" >
+          <p class="text-sm lg:text-lg">View Project</p>
+         <img src="build/img/footer-github.png" alt="github-link"">
+        </div>
+        <p class="m-1 w-4/5 text-xs lg:text-base">${item.desc}</p>
+    </div>`;
+    } else {
+      return `
+      <div class="project-card" class="text-gray-300">
+          <img src= ${item.img} alt=${item.title}>
+          <h2 class="text-xl lg:text-2xl  font-bold mx-3 pt-1 underline underline-offset-8">${item.title}</h2>
+          <div class="flex justify-between w-full px-4  projectLinkContainer items-center" >
+            <p class="text-sm lg:text-lg">View Project</p>
+           <img src="build/img/footer-github.png" alt="github-link"">
+          </div>
+          <p class="m-1 w-4/5 text-xs lg:text-base">${item.desc}</p>
+      </div>`;
+    }
+  });
+  displayProjects = displayProjects.join("");
+  projectContainer.innerHTML = displayProjects;
+  setTimeout(() => {
+    var player1, firstStateChange;
+    player1 = new YT.Player("player-1", {
+      videoId: "KLc7isg2IyA",
     });
-    displayProjects = displayProjects.join("");
-    projectContainer.innerHTML = displayProjects;
-
+  }, 1000)
 }
 
-
-window.addEventListener("DOMContentLoaded", function(){
-    displayAllProjects(projectList);
-})
+window.addEventListener("DOMContentLoaded", function () {
+  displayAllProjects(projectList);
+});
